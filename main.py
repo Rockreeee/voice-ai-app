17a67262eb56099dfcb7c06e13b2d38f8102e552
Rockreeee <mmakt122@gmail.com>
2024-04-16 15:38:22 +0900

    update

--- main.py
+++ main.py
@@ -4,35 +4,34 @@ import text_to_speech as speech
 
 gpt_start_flag = True
 
-def main():
-    # 対話の開始
-    while True:
+# 対話の開始
+while True:
 
-        # 音声認識を実行
-        recognized_text = vc.recognize_speech()
-        print("You said: ", recognized_text)
+    # 音声認識を実行
+    recognized_text = vc.recognize_speech()
+    print("You said: ", recognized_text)
 
-        if recognized_text == "exit":
-            speech.speak("Thank you")
-            break
+    if recognized_text == "exit":
+        speech.speak("Thank you")
+        break
 
-        if recognized_text == "stop":
-            speech.speak("GPT stop")
-            gpt_start_flag = False
+    if recognized_text == "stop":
+        speech.speak("GPT stop")
+        gpt_start_flag = False
 
-        # gptの動作許可がある場合は動く
-        if gpt_start_flag and recognized_text != "":
-            # chat gptに入力
-            user_input = recognized_text
-            response = gpt.interact_with_chatgpt(user_input)
-            print("Gpt said: ", response)
+    # gptの動作許可がある場合は動く
+    if gpt_start_flag and recognized_text != "":
+        # chat gptに入力
+        user_input = recognized_text
+        response = gpt.interact_with_chatgpt(user_input)
+        print("Gpt said: ", response)
 
-            # テキストを音声に変換して再生
-            speech.speak(response)
+        # テキストを音声に変換して再生
+        speech.speak(response)
 
-        if recognized_text == "start":
-            speech.speak("GPT start")
-            gpt_start_flag = True
+    if recognized_text == "start":
+        speech.speak("GPT start")
+        gpt_start_flag = True
 
-if __name__ == "__main__":
-    main()
+
+
